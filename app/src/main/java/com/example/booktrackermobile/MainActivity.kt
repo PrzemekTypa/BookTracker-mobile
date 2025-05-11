e5d302df2bcf844d3f633e2849b36712199fb1ab
PrzemekTypa <przemyslaw.typa@studenci.collegiumwitelona.pl>
2025-05-11 17:23:03 +0200

    login screen

--- app/src/main/java/com/example/booktrackermobile/MainActivity.kt
+++ app/src/main/java/com/example/booktrackermobile/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.booktrackermobile.ui.theme.BookTrackerMobileTheme
+import com.example.booktrackermobile.screens.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BookTrackerMobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginScreen()
             }
         }
     }
